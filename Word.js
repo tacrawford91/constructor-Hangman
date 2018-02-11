412e1db954eedf603f988e01d60b714d0afbd1ad
Troy Crawford <tacrawford91@gmail.com>
2018-02-11 15:45:31 -0600

    Fixed starting screen. Deleted word1 and moved all info into word.

--- Word.js
+++ Word.js
@@ -1,135 +1,43 @@
-const inquirer = require("inquirer");
 const Letter = require("./Letter.js");
 
 
-var checkArray = []
-var turnsRemaining = 10;
-var guessCorrect = false 
-// var displayArray = [];
-//word to guess be jurassic 
+function Word (newWord) {
+    this.wordArray = newWord.split('').map(x => new Letter(x)),
+    this.secretWord = newWord,
 
-function Word () {
-    this.wordArray = [
-        new Letter ("j"),
-        new Letter ("u"),
-        new Letter ("r"),
-        new Letter ("a"),
-        new Letter ("s"),
-        new Letter ("s"),
-        new Letter ("i"),
-        new Letter ("c")
-    ],
-    this.returnString = function() {
-        var displayArray = []
+    this.displayWord = function() {
+        var displayWord = [] 
         this.wordArray.forEach(function(element) {
-                    // element.show();
-                displayArray.push(element.show())
-                //check if the elemet has been guessed
-                // if (element.letterGuessed) {
-                //     displayArray.push(element.char)
-                //     console.log(`element letter guessed: ${element.letterGuessed}`)
-                //     guessCorrect = true;
-                // } else {
-                //     displayArray.push("_");
-                // }
+            displayWord.push(element.show());
         })
-        console.log(displayArray.join(" "))
-        // console.log(`guess correct : ${guessCorrect}`)
-        // if (guessCorrect === true) {
-        // console.log(`Correct!!
-        // turns remaining: ${turnsRemaining}
-        // ${displayArray.join(" ")}`); 
-        // // return displayArray.join("")
-        // // guessCorrect = false
-        // console.log(guessCorrect);
-        // } 
-        // if (guessCorrect === false) {
-        //     turnsRemaining--
-        //     console.log(`INCORRECT :(
-        //     turns remaining: ${turnsRemaining}
-        //     ${displayArray.join(" ")}`);
-        //     console.log(guessCorrect);
-        // }
+        console.log(`${displayWord.join(" ")}\n`);
+
     },
-    this.userCharCheck = function (userGuess) {
+    this.checkUserGuess = function (userGuess){
+        var wordGuessed = false;
         this.wordArray.forEach(function(element){
-            element.check(userGuess);
-            if (element.check(userGuess)) {
-            checkArray.push(element.char)
-            console.log(`Check array is ${checkArray}`)
-            // console.log(`guess correct : ${guessCorrect}`)
-            console.log(`Correct!!
-            turns remaining: ${turnsRemaining}`)
+            if (userGuess === element.char) {
+                //change value of property to display letter not _
+                element.letterGuessed = true
+                //change wordguessed to true to alert that a letter was guessed
+                wordGuessed = true
             }
-            })
-            // if (!this.wordArray.incudes(userGuess)){
-            //     turnsRemaining--
-            //     console.log(`Incorrect :(
-            //     turns remaining: ${turnsRemaining}`)
-            //     }
-            // }
+        })
+        if (wordGuessed === true) {
+            //update display
+            this.displayWord();
+            return true
+        }   
+        if (wordGuessed === false) {
+             //update display
+            this.displayWord();
+            return false
+        }
     }
-    
-
-
-// function Word (letter) {
-//     this.wordArray = [
-//         new Letter (Letter)],
-//     this.returnString = function() {
-//         var displayArray = []
-//         this.wordArray.forEach(function(element) {
-//             // element.show();
-//            //check if the elemet has been guessed
-//            if (element.letterGuessed) {
-//                displayArray.push(element.char)
-//            } else {
-//                displayArray.push("_");
-//            }
-//         })
-//         console.log(`${displayArray.join(" ")}`); 
-//         // return displayArray.join("")
-//     },
-//     this.userCharCheck = function (userGuess) {
-//         this.wordArray.forEach(function(element){
-//             element.check(userGuess);
-//         })
-//     }
-// }
+}
 
 module.exports = {
-	Word: Word,
-    checkArray: checkArray,
-    turnsRemaining: turnsRemaining,
-    guessCorrect: guessCorrect
-};
+    Word: Word,
+}
 
-// var jurassic = new Word("j","u","r","a","s","s","i","c");
 
-// console.log(jurassic.wordArray);
-
-// // jurassic.returnString();
-
-// var counter = 5;
-
-// function playGame() {
-//     // if (counter = 5) {
-//     // console.log(jurassic.returnString())
-//     // }
-//     inquirer.prompt(
-//         {
-//             type: "input",
-//             name: "userGuess",
-//             message: "Guess a Letter: "
-//         }
-//     ).then(function(answer) {
-//     var userGuess = answer.userGuess
-//     jurassic.userCharCheck(userGuess);
-//     jurassic.returnString();
-//     counter--
-//     if (counter > 0) {
-//         playGame()
-//     } 
-
-//     })
-// }
-// playGame(
--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 const inquirer = require("inquirer");
-const wordJS = require("./Word1.js");
+const wordJS = require("./Word.js");
 
 
-var indexTurns = 7
-var wordList = ["jurassic","scott", "smells", "desktop", "boiler maker"]
+var indexTurns = 7;
+var wordList = ["jurassic","scott", "smells", "desk top", "boiler maker"];
 var wordCounter = Math.floor(Math.random()*wordList.length);
 var currentLevel = new wordJS.Word(wordList[wordCounter]);
 //store guessed letters
-var lettersGuessed = []
-var correctLetters = []
+var lettersGuessed = [];
+var correctLetters = [];
+var firstGame = false;
 
 
 function playGame() {
     //show display for firt time 
-    if(indexTurns === 7) {
-        console.log("LETS PLAY NODE HANGMAN!")
+    if(indexTurns === 7 && firstGame === false) {
+        console.log("LETS PLAY NODE HANGMAN!");
         currentLevel.displayWord();
+        firstGame = true;
     }
     inquirer.prompt(
         {
@@ -25,30 +27,30 @@ function playGame() {
         }
     ).then(function(answer) {
         //stpre users guess
-    var userGuess = answer.userGuess
-    var indexCheckArray = wordJS.checkArray
-
+    var userGuess = answer.userGuess;
+    var indexCheckArray = wordJS.checkArray;
     //check to see if the letter has been guessed if not store if it has send message and replay turn
         if (lettersGuessed.includes(userGuess)) {
-            console.log(`${userGuess}: already guessed, try again`)
+            console.log(`${userGuess}: already guessed, try again`);
             playGame();
         } else {
             //push letter guessed into array 
             lettersGuessed.push(userGuess);
         //check user guess to each letter, set value to true if match
         if (currentLevel.checkUserGuess(userGuess) === true) {
-            // console.log(`LOOOKIE HERE IS THIS BEING RAN TWICE -  - ${currentLevel.checkUserGuess(userGuess)}`)
             //push user guess into correctletter array
             currentLevel.secretWord.split("").forEach(function(element){
                 if(userGuess === element) {
                     correctLetters.push(userGuess);
                 }
+                if (element === "") {
+                    correctLetters.push(element);
+                }
             })
             //correct
             console.log(`correct! \n Turns Remaining: ${indexTurns}
             `)
-            gameScoreLogic()
-            // (currentLevel.checkUserGuess(userGuess) === false)
+            gameScoreLogic();
         } else {
             indexTurns--
             console.log(`Incorrect :( \n Turns Remaining: ${indexTurns}
@@ -66,23 +68,17 @@ playGame()
 
 function gameScoreLogic() {
     if (indexTurns> 0 && correctLetters.length < currentLevel.secretWord.length) {
-        // console.log(correctLetters.length)
-        // console.log("secret word len"+currentLevel.secretWord.length)
-        // console.log("turns from index" + indexTurns)
-
         playGame()
     }
     if (indexTurns> 0 && correctLetters.length === currentLevel.secretWord.length) {
-        console.log("winner winner chicken dinner");
-        console.log("Next Word Generate");
+        console.log("winner winner chicken dinner. Next word Generated. Good Luck");
+                
         //remove last word from word array
-        console.log(wordList) 
         wordList.splice(wordCounter,1);
         if (wordList.length === 0) {
             console.log("Congrats! You beat the entire game. Have a drink. You deserve it!"); 
             return
         }
-        console.log(wordList);
         wordCounter = Math.floor(Math.random()*wordList.length);
         //generate new word
         currentLevel = new wordJS.Word(wordList[wordCounter]);
@@ -93,6 +89,7 @@ function gameScoreLogic() {
         // empty letters Guessed
         lettersGuessed = [];
         //playGame
+        currentLevel.displayWord();
         playGame();
     }
     if (indexTurns === 0 && correctLetters.length !== currentLevel.secretWord.length) {
@@ -100,65 +97,3 @@ function gameScoreLogic() {
 
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function playGame() {
-//     // if (counter = 5) {
-//     // console.log(jurassic.returnString())
-//     // }
-//     inquirer.prompt(
-//         {
-//             type: "input",
-//             name: "userGuess",
-//             message: "Guess a Letter: "
-//         }
-//     ).then(function(answer) {
-//         //stpre users guess
-//     var userGuess = answer.userGuess
-//     //check user guess to each letter, set value to true if match
-//     jurassic.userCharCheck(userGuess);
-//     //update display 
-//     jurassic.returnString();
-//     // turnsRemaining--
-//     if (indexTurns> 0) {
-//         // check if word is complete
-//        if(correctLetters.length === jurassic.wordArray.length) {
-//            console.log("HORRRAYYYYYYYY")
-//        };
-//         playGame()
-//     }
-
-
-
-//                                                 // } else if ( turnsRemaining > 0 && !completeWordCheck.includes("_")) {
-//                                                 //     console.log(!completeWordCheck.includes("_"))
-//                                                 //     console.log(completeWordCheck);
-//                                                 //     console.log(`winner winner chicken dinner`);
-//                                                 // } else if (turnsRemaining === 0 && completeWordCheck.includes("_")){
-//                                                 //     console.log(`you lose`)
-//                                                 // }
-
-//     })
-// }
-// playGame()
